5a22d595946d30bb904160761e0de485b7476b93
sscodersen <marketing.businessblueprint@gmail.com>
2023-01-17 15:08:04 -0800

    wap

--- server/bobba/catalogue/Catalogue.ts
+++ server/bobba/catalogue/Catalogue.ts
@@ -2,6 +2,7 @@ import CataloguePage from "./CataloguePage";
 import BobbaEnvironment from "../BobbaEnvironment";
 import RequestCataloguePage from "../communication/outgoing/catalogue/RequestCataloguePage";
 import RequestCataloguePurchase from "../communication/outgoing/catalogue/RequestCataloguePurchase";
+import { ethers } from 'ethers';
 
 export type CatalogueIndex = {
     id: number;
@@ -15,18 +16,26 @@ export type CatalogueIndex = {
 export default class Catalogue {
     catalogueIndex: CatalogueIndex[];
     pages: { [id: number]: CataloguePage };
+    nftContract: ethers.Contract;
+    player: Player;
 
     constructor() {
         this.catalogueIndex = [];
         this.pages = {};
     }
 
-    setIndex(index: CatalogueIndex[]) {
+    setIndex(index: CatalogueIndex[], contractAddress: string) {
         this.catalogueIndex = index;
         BobbaEnvironment.getGame().uiManager.onLoadCatalogueIndex(index);
         if (index.length > 0) {
             this.requestPage(index[0].id);
         }
+        // Connect to the NFT contract
+        const provider = new ethers.providers.Web3Provider(web3.currentProvider);
+        this.nftContract = new ethers.Contract(contractAddress, abi, provider.getSigner());
+    }
+    setPlayer(player: Player){
+        this.player = player;
     }
 
     setCataloguePage(page: CataloguePage) {
@@ -47,7 +56,39 @@ export default class Catalogue {
         }
     }
 
-    requestPurchase(pageId: number, itemId: number) {
-        BobbaEnvironment.getGame().communicationManager.sendMessage(new RequestCataloguePurchase(pageId, itemId));
-    }
-}
+    async requestPurchase(itemId: number) {
+        try {
+            // Get the price of the item
+            const price = await this.nftContract.functions.getPrice(itemId);
+
+            // Check if the player has enough currency
+            if (this.player.currency >= price) {
+                // Buy the item
+                await this.nftContract.functions.buy(itemId, { value: price });
+
+                // Add the item to the player's inventory
+                this.player.inventory.push(itemId);
+
+                // Update the player's currency and inventory text
+                this.player.currency -= price;
+                this.player.updateInventory();
+            } else {
+                alert('You do not have enough currency to buy this item.');
+            }
+        }catch (error) {
+            console.error(error);
+            }
+            }
+            }
+
+            class Player {
+            inventory: number[];
+            currency: number;
+            updateInventory: Function;constructor() {
+                this.inventory = [];
+                this.currency = 0;
+            }
+            setUpdateInventory(callback: Function) {
+                this.updateInventory = callback;
+            }
+        }
--- server/bobba/catalogue/CatalogueItem.ts
+++ server/bobba/catalogue/CatalogueItem.ts
@@ -1,6 +1,7 @@
 import { ItemType } from "../imagers/furniture/FurniImager";
 import BobbaEnvironment from "../BobbaEnvironment";
 import BaseItem from "../items/BaseItem";
+import { ethers } from 'ethers';
 
 export default class CatalogueItem {
     itemId: number;
@@ -10,8 +11,9 @@ export default class CatalogueItem {
     baseId: number;
     baseItem: BaseItem | null;
     amount: number;
+    nftContract: ethers.Contract;
 
-    constructor(itemId: number, itemName: string, cost: number, itemType: ItemType, baseId: number, amount: number) {
+    constructor(itemId: number, itemName: string, cost: number, itemType: ItemType, baseId: number, amount: number, contractAddress: string) {
         this.itemId = itemId;
         this.itemName = itemName;
         this.cost = cost;
@@ -19,6 +21,30 @@ export default class CatalogueItem {
         this.baseId = baseId;
         this.amount = amount;
         this.baseItem = null;
+        // Connect to the NFT contract
+        const provider = new ethers.providers.Web3Provider(web3.currentProvider);
+        this.nftContract = new ethers.Contract(contractAddress, abi, provider.getSigner());
+    }
+
+    async requestPurchase(player: Player) {
+        try {
+            // Check if the player has enough currency
+            if (player.currency >= this.cost) {
+                // Buy the item
+                await this.nftContract.functions.buy(this.itemId, { value: this.cost });
+
+                // Add the item to the player's inventory
+                player.inventory.push(this.itemId);
+
+                // Update the player's currency and inventory text
+                player.currency -= this.cost;
+                player.updateInventory();
+            } else {
+                alert('You do not have enough currency to buy this item.');
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     loadBase(): Promise<BaseItem> {
