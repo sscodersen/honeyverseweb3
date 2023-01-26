fa921f4f98405fbb62c1343a55bbba30b1bd1d13
sscodersen <marketing.businessblueprint@gmail.com>
2023-01-26 14:16:18 -0800

    whoopwhoop

--- client/src/scenes/startScene.ts
+++ client/src/scenes/startScene.ts
@@ -1,8 +1,6 @@
 import WalletConnectProvider from "@walletconnect/web3-provider"
 import { ethers } from "ethers"
 import Web3Modal from "web3modal"
-import generateTypedAuth from "../../../commons/auth.mjs"
-import { BTN_GREY, CONNECT_SCENE, SIGNER, UPHEAVAL } from "../utils/keys"
 
 const connectWallet = async () => {
     const providerOptions = {
@@ -12,9 +10,7 @@ const connectWallet = async () => {
                 infuraId: "0e7fcc143f894d179aa51dbdc44d8ac5"
             }
         },
-        
     }
-
     const web3Modal = new Web3Modal({
         providerOptions
     })
@@ -26,17 +22,11 @@ const connectWallet = async () => {
     instance = await web3Modal.connect()
 
     const provider = new ethers.providers.Web3Provider(instance)
-
     const signer = provider.getSigner()
     return signer
 }
 
 export class StartScene extends Phaser.Scene {
-    button?: Phaser.GameObjects.RenderTexture
-    text?: Phaser.GameObjects.BitmapText
-    container?: Phaser.GameObjects.Container
-    signer?: ethers.providers.JsonRpcSigner
-
     constructor() {
         super({
             key: 'start-scene'
@@ -44,86 +34,11 @@ export class StartScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.bitmapFont(UPHEAVAL, '/fonts/upheaval.png', '/fonts/upheaval.xml')
-        this.load.image(BTN_GREY, '/ui/btn-grey.png')
     }
 
     create() {
-        //set bg color
-        this.cameras.main.setBackgroundColor('0x171717')
-
-        //get screen height and width
-        const { width, height } = this.scale
-
-        //add components
-        this.button = this.add.nineslice(0, 0, 100, 18, BTN_GREY, [3, 3, 5, 3]).setOrigin(0.5, 0.5).setScale(3, 3).setInteractive()
-        this.text = this.add.bitmapText(0, 0, UPHEAVAL, 'connect wallet', 32).setOrigin(0.5, 0.5)
-        this.container = this.add.container(width * 0.5, height * 0.5, [this.button, this.text])
-
-        //add event listeners
-        this.scale.on('resize', () => this.resize())
-        this.button.on('pointerover', () => {
-            this.button?.setTint(0x44fff9)
-        })
-        this.button.on('pointerout', () => {
-            this.button?.clearTint()
-        })
-        this.button.on('pointerdown', () => {
-            this.button?.setTint(0x2aa19d)
-        })
-        this.button.on('pointerup', async () => {
-            this.button?.clearTint()
-
-            if (!this.signer) {
-                try {
-                    this.signer = await connectWallet()
-                    if (this.signer) {
-                        this.registry.set(SIGNER, this.signer)
-                        this.text?.setText('login')
-                    } else {
-                        throw new Error('no provider')
-                    }
-                    return
-                } catch (e) {
-                    console.error(e)
-                    return
-                }
-            }
-
-            //login
-            if (this.signer) this.authenticate()
-        })
-    }
-
-    resize() {
-        //recenter on resize
-        const { width, height } = this.scale
-        this.container?.setPosition(width * 0.5, height * 0.5)
-    }
-
-    async authenticate() {
-        try {
-            const address = await this.signer?.getAddress()
-
-            const host = import.meta.env.VITE_HOST ? import.meta.env.VITE_HOST : "http://localhost"
-
-            //get challenge
-            const res = await fetch(host + "/challenge", {
-                method: "POST",
-                body: address,
-            })
-            if (!res.ok) {
-                throw new Error(`${res.status} ${res.status}`)
-            }
-            const challenge = await res.text()
-            const { domain, types, value } = generateTypedAuth(challenge)
-
-            //generate signature
-            const sig = await this.signer?._signTypedData(domain, types, value)
-            
-            this.scene.start(CONNECT_SCENE, { sig, address })
-        } catch (e) {
-            console.error(e)
-        }
+        connectWallet().then((signer) => {
+            this.scene.start('dungeon-scene', { signer });
+        });
     }
 }
